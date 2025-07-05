98f6775f2fa1825ef4fb6035df568a45d5c12c1b
4sterisk <4sterisk@ymail.ne.jp>
2025-07-05 21:03:00 +0900

    ダークテーマの追加

--- script.js
+++ script.js
@@ -1,18 +1,41 @@
 
 window.addEventListener('DOMContentLoaded', () => {
     document.body.classList.add('loaded');
+
+    const themeToggle = document.getElementById('theme-toggle');
+    const body = document.body;
+
+    const currentTheme = localStorage.getItem('theme');
+    if (currentTheme) {
+        body.classList.add(currentTheme);
+        if (currentTheme === 'dark-theme') {
+            themeToggle.checked = true;
+        }
+    } else if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+        body.classList.add('dark-theme');
+        themeToggle.checked = true;
+    }
+
+    themeToggle.addEventListener('change', () => {
+        body.classList.toggle('dark-theme');
+        let theme = 'light-theme';
+        if (body.classList.contains('dark-theme')) {
+            theme = 'dark-theme';
+        }
+        localStorage.setItem('theme', theme);
+    });
 });
 
-document.getElementById('dialog-button').addEventListener('click', function () {
-    document.getElementById('dialog').style.display = 'flex'; // flexに変更
-});
+document.addEventListener('keydown', (event) => {
+    if (event.key === 'd') {
+        const themeToggle = document.getElementById('theme-toggle');
+        themeToggle.checked = !themeToggle.checked;
+        document.body.classList.toggle('dark-theme');
 
-document.querySelector('.close-button').addEventListener('click', function () {
-    document.getElementById('dialog').style.display = 'none';
-});
-
-window.addEventListener('click', function (event) {
-    if (event.target == document.getElementById('dialog')) {
-        document.getElementById('dialog').style.display = 'none';
+        let theme = 'light-theme';
+        if (document.body.classList.contains('dark-theme')) {
+            theme = 'dark-theme';
+        }
+        localStorage.setItem('theme', theme);
     }
 });
